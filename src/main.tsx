b6e77c8a98f4d66fc4d3591878d85a7591c1b8a5
tuyulgaul-sketch <banjarnainggolan1991@gmail.com>
2026-09-03 16:07:45 +0700

    Auto-set meeting room end time one hour after start

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import { installMeetingRoomAutoDuration } from "@/lib/meetingRoomAutoDuration";
+
+installMeetingRoomAutoDuration();
 
 createRoot(document.getElementById("root")!).render(<App />);
 
